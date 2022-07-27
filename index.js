77f354747cb8495583c3575a1b4aeff2ad5ba2b3
Edris Issaian <edris.issaian@gmail.com>
2022-07-27 14:07:58 +0400

    New Task

--- index.js
+++ index.js
@@ -4,17 +4,67 @@
 //   console.log(numbers[i]);
 // }
 
-const number = +prompt();
-let isPrime = true;
 
-for (let i = 2; i < number; i++) {
 
-  if (number % i === 0) isPrime = false;
-}
 
-if (isPrime) {
-  console.log("Prime Number");
-}
-else {
-  console.log("Composite Number");
-}
+// const number = +prompt("Please add a number");
+// let isPrime = true;
+
+// if (number === 1) {
+//   console.log("1 neither Prime number nor Composite number");
+// } else {
+//   for (let i = 2; i < number; i++) {
+//     if (number % i === 0) {
+//       isPrime = false;
+//       console.log("Not Prime");
+//       break;
+//     }
+//   }
+
+//   if (isPrime) {
+//     console.log("Prime Number");
+//   }
+// }
+
+
+
+
+// const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const input = +prompt("Enter a number");
+
+// for (let i = num.length-1; i >= 0; i--) {
+//     console.log(input*num[i]);
+// }
+
+
+
+
+// // const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // const input = +prompt("Enter a number");
+
+// // for (let i = 0; i < num.length; i++) {
+// //     console.log(input*num[i]);
+// // }
+
+
+
+
+// const number = +prompt("Add a number");
+// let isOdd = true;
+
+// if (number === 0) {
+//   console.log("0 neither Odd nor Even");
+// } 
+// else {
+ 
+//     if (number % 2 === 0) {
+//       isOdd = false;
+//       console.log(`${number} is Even`);
+      
+//     }
+
+//     if (isOdd) {
+//       console.log(`${number} is Odd`);
+//     }
+//   }
+
